324cf951a5f815817ba1305017b748b915292169
dmitr <dimanmosenko@gmail.com>
2023-05-17 22:02:02 +0300

    add

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -8,7 +8,7 @@ import javax.servlet.Filter;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    // Метод, указывающий на класс конфигурации
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{
